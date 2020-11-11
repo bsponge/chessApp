1ecdb64b6766ec9f64b8ec9ba5c4dcbd2ee0dc60
bsponge <kubask03@gmail.com>
2020-11-11 22:15:52 +0100

    new chess logic and test

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,21 +3,17 @@ package com.example.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Slf4j
 @Controller
 @SessionAttributes("player")
 public class HomeController {
-    @ModelAttribute("player")
-    public Player player() {
-        return new Player();
-    }
 
     // szachownica
     @GetMapping("/")
     public String homePage() {
         return "home";
     }
+
 }
