12723cf277b4a8b9e635585f029073d452e38354
bsponge <bsponge@github.com>
2021-01-29 19:11:25 +0100

    Remove useless comments

--- src/main/java/com/example/demo/controllers/ApiController.java
+++ src/main/java/com/example/demo/controllers/ApiController.java
@@ -21,6 +21,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+/*
+    TODO:
+        - undoMove accept
+        - game time
+        - extract websocket endpoints to external class
+ */
+
 @Slf4j
 @RestController
 @CrossOrigin
@@ -103,50 +110,6 @@ public class ApiController {
         } else {
             return new ResponseEntity<>(null, HttpStatus.ACCEPTED);
         }
-        /*if (!id.equals("none")) {
-            UUID playerId;
-            try {
-                playerId = UUID.fromString(id);
-                if (players.containsKey(playerId)) {
-                    Player player = players.get(playerId);
-                    GameSession gameSession;
-                    SidesMessage sidesMessage;
-                    if (gameQueue.isEmpty()) {      // queue of games is empty
-                        gameSession = new GameSession(player);
-                        gameQueue.add(gameSession);
-                        player.setColor(Color.WHITE);
-                        player.setGameSessionId(gameSession.getId());
-                        sidesMessage = new SidesMessage(gameSession.getWhitePlayer().getId().toString(), null);
-                        return new ResponseEntity<>(sidesMessage.toString(), HttpStatus.OK);
-                    } else {                        // queue of games is not empty
-                        gameSession = gameQueue.poll();
-                        gameSession.setBlackPlayer(player);
-                        player.setColor(Color.BLACK);
-                        player.setGameSessionId(gameSession.getId());
-                        gameSessions.put(gameSession.getId(), gameSession);
-                        sidesMessage = new SidesMessage(
-                                gameSession
-                                        .getWhitePlayer()
-                                        .getId()
-                                        .toString(),
-                                gameSession
-                                        .getBlackPlayer()
-                                        .getId()
-                                        .toString());
-                        simpMessagingTemplate
-                                .convertAndSend(
-                                        "/topic/messages/" + gameSession.getId(),
-                                        sidesMessage);
-                        return ResponseEntity.ok(sidesMessage.toString());
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-
-         */
     }
 
     @MessageMapping("/chess/{toGameSession}")
@@ -209,47 +172,5 @@ public class ApiController {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        /*
-        log.info(move);
-        Gson gson = new GsonBuilder()
-                .registerTypeAdapter(MoveMessage.class, new MoveMessageSerializer())
-                .create();
-        MoveMessage moveMessage = gson.fromJson(move, MoveMessage.class);
-        if (moveMessage.getGameUuid() != null) {
-            GameSession gameSession = gameSessions.get(moveMessage.getGameUuid());
-            if (moveMessage.isUndo()) {
-                if (gameSession.getMovesHistory().size() > 0) {
-                    Move m = gameSession.getMovesHistory().get(gameSession.getMovesHistory().size() - 1);
-                    m = new Move(m.getToX(), m.getToY(), m.getFromX(), m.getFromY(), m.getColor(), m.getType(), m.getEnemyColor(), m.getEnemyType());
-                    gameSession.undoLastMove();
-                    MoveMessage mm = new MoveMessage(UUID.fromString(toGameSession), moveMessage.getPlayerUuid(), true, m);
-                    mm.setChecksAndMates(gameSession);
-                    log.info("Undo move");
-                    simpMessagingTemplate.convertAndSend("/topic/messages/" + toGameSession, mm);
-                }
-            } else {
-                Move m = moveMessage.getMove();
-                boolean a = gameSession.getColorTurn() == Color.WHITE ? gameSession.getWhitePlayer().getId().equals(moveMessage.getPlayerUuid()) : Objects.requireNonNull(gameSession).getBlackPlayer().getId().equals(moveMessage.getPlayerUuid());
-                boolean b = gameSession.move(Objects.requireNonNull(m).getFromX(), m.getFromY(), m.getToX(), m.getToY(), null);
-                if (a && b) {
-                    if ((m.getFromX() == 4 && m.getToX() == 6) || (m.getFromX() == 4 && m.getToX() == 2)) {
-                        moveMessage.setCastle(true);
-                    }
-                    log.info("After move");
-                    log.info(gameSession.getColorTurn().toString());
-                    moveMessage.setChecksAndMates(gameSession);
-                    log.info(Boolean.toString(moveMessage.isMateOnBlack()));
-                    log.info(Boolean.toString(moveMessage.isMateOnWhite()));
-                    if (moveMessage.isMateOnBlack() || moveMessage.isMateOnWhite()) {
-                        log.info("SAVING GAME HISTORY");
-                        gamesHistoryService.saveGameHistory(gameSession);
-                    }
-                    simpMessagingTemplate.convertAndSend("/topic/messages/" + toGameSession, moveMessage);
-                }
-            }
-
-        }
-
-         */
     }
 }
--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -52,7 +52,6 @@ public class HomeController {
                 e.printStackTrace();
             }
         }
-
         return "home";
     }
 
